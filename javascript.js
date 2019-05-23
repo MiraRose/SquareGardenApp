36621b3972f0ef444fa3a372eefb92b8a6783d27
Miranda <mirandarregan@gmail.com>
2019-05-22 21:17:19 -0400

    Still learning vue but getting further, now have selection boxes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,54 @@
 
-var app = new Vue({
-    el: '#app',
-    data: {
-        vegetables: [carrot, tomato, george]
-    }
+// var selectionBox = new Vue({
+//     el: '#selectionBox',
+//     data: {
+//       selected: 'Tomato',
+//       options: [
+//         { text: 'Tomato', value: 'Tomato' },
+//         { text: 'Carrot', value: 'Carrot' },
+//         { text: 'Lettuce', value: 'Lettuce' }
+//       ]
+//     }
+//   })
 
-})
+  var example1 = new Vue({
+    el: '#example-1',
+    data: {
+      items: [
+        { message: 'Foo' },
+        { message: 'Bar' }
+      ],
+      selected: 'Tomato',
+      options: [
+        { text: 'Tomato', value: 'Tomato' },
+        { text: 'Carrot', value: 'Carrot' },
+        { text: 'Lettuce', value: 'Lettuce' }
+      ]
+    }
+  })
+
+//   for (i = 0; i < 10; i++) {
+    
+
+//     <div class = "col-sm-3 outerSquareBorder">
+//         <div id = i class = "square">
+//                 <div class="dropdown">
+//                         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                           Add a vegetable.
+//                         </button>
+//                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+//                           <a class="dropdown-item" href="#">Tomato</a>
+//                           <a class="dropdown-item" href="#">Carrot</a>
+//                           <a class="dropdown-item" href="#">Oregano</a>
+//                         </div>
+//                       </div>
+//         </div>
+//     </div> 
+
+//     if (i % 3 = 0) {
+//         </div>
+//         <div class = "row no-gutters">
+
+            
+//     }
+//   }
