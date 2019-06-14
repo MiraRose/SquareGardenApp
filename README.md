d85e4efb4b3143cf0435b6d1d23e6ff54c6724c1
Miranda <mirandarregan@gmail.com>
2019-06-13 20:31:04 -0400

    In middle of making list same veggies count up.'

--- README.md
+++ README.md
@@ -6,15 +6,17 @@ Currently you can:
 2. Selecting a veggie for a square puts suggestions IN NEARBY squares
 3. Get (limited) suggestions in that square for nearby squares
 4. Makes list, but doesn't update list when a square changes.
+5. Have icons for tomatoes, lettuce, and carrots
 
 Planned features, in no particular order:
 
-1. Icons for veggies
+1. More icons for veggies
 2. More vegetables/suggestions
 3. Veggie sizes
     ie: tomatoes should take up 4 squares
     ie: some veggies can take up part of a squara
 4. Being able to sow underneath other veggies (carrots under tomatos)
 5. Custom sized beds
-6. Helpful tips (ie: rabbits really like this, prepare, bury tomatoes 1/2 - 3/4)
+6. Helpful tips (ie: rabbits really like this, prepare, bury tomatoes 1/2 - 3/4, this is why it's good to plant this beside this)
 7. Functioning create and export/print list for shopping
+    - count up of same plant
--- javascript.js
+++ javascript.js
@@ -19,23 +19,44 @@ var app = new Vue({
         ],
 
         veggies: [
+            //selected  //suggested //selected image
             ['Tomato', 'Carrot', 'images/svg/veggies/003-tomato.svg'],
-            ['Carrot', 'Tomato', 'images/svg/veggies/001-carrot.svg']
+            ['Carrot', 'Tomato', 'images/svg/veggies/001-carrot.svg'],
+            ['Lettuce', 'Broccoli, carrots, dill, cilantro', 'images/svg/veggies/036-lettuce.svg'],
+            ['Dill', 'broccoli, cabbage, corn, eggplant, lettuce, onions, cucumbers', 'images/svg/veggies/dill.svg']
+        ],
+
+        listOfVeggies: [
+            
         ]
         
     },
+
     created: function () {
-        var i = 0;
-        var x = 0;
-        for (i = 0; i < 4; i++) {
+        
+        for (var i = 0; i < 4; i++) {
             this.plots.push([]);
-            for (x = 0; x < 4; x++) {
+            for (var x = 0; x < 4; x++) {
                 this.plots[i].push({ selected: '', suggestion: 'none', partner: '' , image: 'images/svg/veggies/dirt.svg'});
             }
         }
     },
+
     methods: {
-        
+        listMaker: function (plot) {
+            // for (var i = 0; i < this.plots.length; i++) {
+            //     if (this.listOfVeggies.length > 0 && this.plots.selected != '') {
+            //     for (var x = 0; x < this.listOfVeggies; x++) {
+            //         if (this.plots.selected == this.listOfVeggies[0]) {
+            //             this.listOfVeggies[1] = this.listOfVeggies[1] + 1;
+            //         }
+            //     }
+            // }
+            // else {
+                this.listOfVeggies.push({text: plot.selected, count: 1})
+            // }
+            // }
+        },
         veggiePlanter: function (plot, index1, index2) {
             
             for (var i = 0; i < this.veggies.length; i++) {
@@ -44,15 +65,6 @@ var app = new Vue({
                     plot.image = this.veggies[i][2];
                 }
             }
-            // if (plot.selected == 'Carrot') {
-            //     plot.partner = 'Tomato';
-            //     plot.image = 'images/svg/veggies/001-carrot.svg';
-            // }
-            // else if (plot.selected == 'Tomato') {
-            //     plot.partner = 'Carrot';
-            //     plot.image = 'images/svg/veggies/003-tomato.svg';
-            // }
-            
 
             if (index2 + 1 < this.plots[index1].length) {
                 this.plots[index1][index2 + 1].suggestion = plot.partner;
