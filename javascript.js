e3d15fd5bf76cfab76742af4ca190f51f5e4bc10
MiraRose <mirandarregan@gmail.com>
2019-07-02 21:14:24 -0400

    Added data for companion plants and some neatening up, text on the menu

--- javascript.js
+++ javascript.js
@@ -22,28 +22,35 @@ var app = new Vue({
 
         veggies: [
             //selected  //suggested //selected image
-            ['Tomato', 'Carrot', 'images/svg/veggies/003-tomato.svg'],
-            ['Carrot', 'Tomato', 'images/svg/veggies/001-carrot.svg'],
-            ['Lettuce', 'Broccoli, carrots, dill, cilantro', 'images/svg/veggies/036-lettuce.svg'],
+            ['Tomato', 'Asparagus, basil, beans, marigold, garlic, corn, dill, carrots, eggplant', 'images/svg/veggies/003-tomato.svg'],
+            ['Carrot', 'Lettuce, onion, beans, strawberries', 'images/svg/veggies/001-carrot.svg'],
+            ['Lettuce', 'Broccoli, carrots, dill, radish, onions, beans, broccoli', 'images/svg/veggies/036-lettuce.svg'],
             ['Dill', 'broccoli, cabbage, corn, eggplant, lettuce, onions, cucumbers', 'images/svg/veggies/dill.svg'],
-            ['Pumpkin', 'none', 'images/svg/veggies/002-pumpkin.svg'],
-            ['Watermelon', 'none', 'images/svg/veggies/006-watermelon.svg'],
-            ['Green Onions', 'none', 'images/svg/veggies/010-spring-onion.svg'],
-            ['Eggplant', 'none', 'images/svg/veggies/011-eggplant.svg'],
-            ['Bell Pepper', 'none', 'images/svg/veggies/012-bell-pepper.svg'],
-            ['Potato', 'none', 'images/svg/veggies/013-potato.svg'],
-            ['Strawberries', 'none', 'images/svg/veggies/016-strawberry.svg'],
-            ['Broccoli', 'none', 'images/svg/veggies/018-broccoli.svg'],
-            ['Radish', 'none', 'images/svg/veggies/021-radish.svg'],
-            ['Peas', 'none', 'images/svg/veggies/022-peas.svg'],
-            ['Onion', 'none', 'images/svg/veggies/023-onion.svg'],
-            ['Chili Pepper', 'none', 'images/svg/veggies/028-chili.svg'],
-            ['Garlic', 'none', 'images/svg/veggies/034-garlic.svg'],
-            ['Cantalope', 'none', 'images/svg/veggies/035-melon.svg'],
-            ['Corn', 'none', 'images/svg/veggies/040-corn.svg'],
-            ['Cabbage', 'none', 'images/svg/veggies/042-cabbage.svg'],
-            ['Cucumber', 'none', 'images/svg/veggies/044-cucumber.svg'],
-            ['Asparagus', 'none', 'images/svg/veggies/048-asparagus.svg']
+            ['Pumpkin', 'Radishes', 'images/svg/veggies/002-pumpkin.svg'],
+            ['Watermelon', 'Corn', 'images/svg/veggies/006-watermelon.svg'],
+            ['Green Onions', 'Carrots, dill, lettuce, marigolds, tomatos', 'images/svg/veggies/010-spring-onion.svg'],
+            ['Eggplant', 'Marigold, dill, beans', 'images/svg/veggies/011-eggplant.svg'],
+            ['Pepper', 'Beans, tomatos, onions, basil', 'images/svg/veggies/012-bell-pepper.svg'],
+            ['Potato', 'Beans, marigolds, peas, onion, garlic', 'images/svg/veggies/013-potato.svg'],
+            ['Strawberries', 'Bush beans, onions', 'images/svg/veggies/016-strawberry.svg'],
+            ['Broccoli', 'Dill, lettuce, onions, tomato, spinach', 'images/svg/veggies/018-broccoli.svg'],
+            ['Radish', 'Lettuce', 'images/svg/veggies/021-radish.svg'],
+            ['Peas', 'Garlic', 'images/svg/veggies/022-peas.svg'],
+            ['Onion', 'Carrots, chamomile, strawberries, dill, lettuce, marigolds, tomatos', 'images/svg/veggies/023-onion.svg'],
+            ['Chili Pepper', 'Beans, tomatos, onions', 'images/svg/veggies/028-chili.svg'],
+            ['Garlic', 'Peas', 'images/svg/veggies/034-garlic.svg'],
+            ['Cantalope', 'Corn', 'images/svg/veggies/035-melon.svg'],
+            ['Corn', 'Dill, beans, potatos', 'images/svg/veggies/040-corn.svg'],
+            ['Cabbage', 'Beans, chamomile, marigold, dill, tomato', 'images/svg/veggies/042-cabbage.svg'],
+            ['Cucumber', 'Radishes, chamomile, marigold, peas, beans, carrots, dill, onions, garlic', 'images/svg/veggies/044-cucumber.svg'],
+            ['Asparagus', 'Dill, tomatoes, basil', 'images/svg/veggies/048-asparagus.svg'],
+            ['Marigold', 'Most plants', 'images/svg/veggies/marigold.svg'],
+            ['Beans', 'Cucumbers, cabbage, carrots, corn, marigolds, potato, radish, lettuce, onion', 'images/svg/veggies/legumes.svg'],
+            ['Spinach', 'Strawberries, peas, beans', 'images/svg/veggies/legumes.svg'],
+            ['Basil', 'Chamomile', 'images/svg/veggies/002-basil.svg'],
+            ['Chamomile', 'Chamomile', 'images/svg/veggies/007-chamomile.svg']
+
+
         ],
 
         listOfVeggies: [
@@ -57,7 +64,12 @@ var app = new Vue({
         for (var i = 0; i < this.veggies.length; i++) {
             this.options.push({ text: this.veggies[i][0], value: this.veggies[i][0] })
         }
-        
+
+        this.options.sort(function(a, b) {
+            if(a.text < b.text) { return -1; }
+            if(a.text > b.text) { return 1; }
+             return 0;
+        })
 
         for (var i = 0; i < 4; i++) {
             this.plots.push([]);
@@ -89,11 +101,7 @@ var app = new Vue({
             }
 
             this.listOfVeggies = _u.countBy(listOfJustVeggies);
-            // var counts = {};
-            // for (var i = 0; i < arr.length; i++) {
-            //     var num = listOfJustVeggies[i];
-            //     counts[num] = counts[num] ? counts[num] + 1 : 1;
-            // }
+            
         },
         veggiePlanter: function (plot, index1, index2) {
 
